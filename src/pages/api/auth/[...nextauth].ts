c90b4f3157120097fe55160ec2b7f1242c5e4e9e
Aron Reis <aronreis2@gmail.com>
2021-11-28 18:37:05 +0000

    :bug: Fix wrong environment variable name

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
+      clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
