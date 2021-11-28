becaf9d6c28ddc87989710e9bfc7225a11ab0741
Aron Reis <aronreis2@gmail.com>
2021-11-28 17:17:36 +0000

    :sparkles: Add API\NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,51 @@
+import { query as q } from 'faunadb'
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import { fauna } from '../../../services/fauna'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user'
+    }),
+  ],
+  // jwt: {
+  //   signingKey: process.env.SIGNING_KEY,
+  // },
+  callbacks: {
+    async signIn(user, account, profile){
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
+})
