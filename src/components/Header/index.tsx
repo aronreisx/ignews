69d92d8d25369e3bdf89664e6ee2cf67ec5db692
aronreisx <aronreis2@gmail.com>
2022-03-20 14:08:58 +0000

    🐛Use Next Image instead of img tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -5,7 +6,7 @@ export function Header(){
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Image src="/images/logo.svg" alt="ig.news" width="108.45px" height="30.27px"/>
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
