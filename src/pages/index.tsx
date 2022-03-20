e86e999e66125a7dcf7d2cfca98f229585cfbd26
aronreisx <aronreis2@gmail.com>
2022-03-20 14:14:33 +0000

    🚧 Update Home and use Next staticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,55 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
-      <h1>
-        Hello World
-      </h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋🏼 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for &euro;{product.amount} per month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <Image src="/images/avatar.svg" alt="Girl coding" width="334px" height="520px"/>
+      </main>
     </>
   )
+}
+
+// Get Price from Stripe API to generate the data
+// every 24 hours - as defined at Revalidate attribute
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JsdEUHbpEHBNboSqh6dw0ed')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-PT', {
+      minimumFractionDigits: 2
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
 }
