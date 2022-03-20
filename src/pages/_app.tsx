6e0d72df604cd4aa2d06dbe733d3529100516111
aronreisx <aronreis2@gmail.com>
2022-03-20 14:22:31 +0000

    ✨ Implement NextAuthProvider and Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import { AppProps } from 'next/app'
-
+import { Provider as NextAuthProvider } from 'next-auth/client';
+import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 import '../styles/global.scss';
 
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  )
 }
 
 export default MyApp
