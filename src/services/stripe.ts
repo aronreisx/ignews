72f5d9939201da541cb68bd73587380ede76106c
Aron Reis <aronreis2@gmail.com>
2022-03-26 21:41:07 +0000

    :pencil2: fix stripe env variable typo

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import { version } from '../../package.json'
 
 export const stripe = new Stripe(
-  process.env.STrIPE_API_KEY,
+  process.env.STRIPE_API_KEY,
   {
     apiVersion: '2020-08-27',
     appInfo: {
